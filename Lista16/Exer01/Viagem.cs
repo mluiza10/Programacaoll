de019c1af526cf959fdb8945f7d51cf4d0d819b0
Maria Luiza de Oliveira Cruz <marialuizaoliveiracruz23@outlook.com>
2019-08-21 11:59:36 -0300

    Lista 16

---
+++ Lista16/Exer01/Viagem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer01
+{
+    class Viagem
+    {
+        private double distancia;
+        public void SetDistancia(double d) { distancia = d; }
+        public double GetDistancia() { return distancia; }
+        private double tempo;
+        public void SetTempo(double t) { tempo = t; }
+        public double GetTempo() { return tempo; }
+        public double CalcVelocidade() { return distancia / tempo; }
+    }
+}
---
+++ Lista16/Exer02/Esfera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer02
+{
+    class Esfera
+    {
+       
+            private double raio;
+            public void SetRaio(double r)
+            {
+                raio = r;
+            }
+            public double GetRaio()
+            {
+                return raio;
+            }
+            public double CalcArea()
+            {
+                return 4 * Math.PI * raio * raio;
+            }
+            public double CalcVolume()
+            {
+                return 4 * Math.PI * raio * raio / 3;
+            }
+    }
+}
