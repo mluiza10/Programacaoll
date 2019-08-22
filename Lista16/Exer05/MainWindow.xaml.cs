53477809af9fdb3d7f0c038c463e2cc204603a20
Maria Luiza de Oliveira Cruz <marialuizaoliveiracruz23@outlook.com>
2019-08-22 15:30:22 -0300

    Resto das questões

---
+++ Lista16/Exer05/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exer05
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Numeros n = new Numeros();
+            n.SetInicio(int.Parse(txtI.Text));
+            n.SetFim(int.Parse(txtF.Text));
+            bool bp, bi;
+            if (check1.IsChecked == true) bp = true;
+            else bp = false;
+            if (check2.IsChecked == true) bi = true;
+            else bi = false;
+            lista.ItemsSource = n.Calc(bp, bi);
+        }
+    }
+}
---
+++ Lista16/Exer05/Numeros.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer05
+{
+    class Numeros
+    {
+        private int inicio, fim;
+
+        public void SetInicio(int i)
+        {
+            inicio = i;
+        }
+
+        public int GetInicio()
+        {
+            return inicio;
+        }
+
+        public void SetFim(int f)
+        {
+            fim = f;
+        }
+
+        public int GetFim()
+        {
+            return fim;
+        }
+
+        public int[] Calc(bool p, bool t)
+        {
+            int[] z = new int[fim - inicio + 1];
+            int cont = 0;
+            for (int x = inicio; x <= fim; x++)
+            {
+                if (p == true && t == false && x % 2 == 0)
+                {
+                    z[cont] = x;
+                    cont++;
+                }
+
+                if (t == true && p == false && x % 2 != 0)
+                {
+                    z[cont] = x;
+                    cont++;
+                }
+
+                if (p == true && t == true)
+                {
+                    z[cont] = x;
+                    cont++;
+                }
+            }
+            int[] w = new int[cont];
+            Array.Copy(z, w, cont);
+            return w;
+        }
+    }
+}
+
