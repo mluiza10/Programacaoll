c22219c2bbdf162cd9ce32961b3e3ca2264dfc4a
Maria Luiza de Oliveira Cruz <marialuizaoliveiracruz23@outlook.com>
2019-08-29 14:48:05 -0300

    Falta a 8

--- Lista16/Exer06/Bingo.cs
+++ Lista16/Exer06/Bingo.cs
@@ -8,5 +8,38 @@ namespace Exer06
 {
     class Bingo
     {
+        private int numBolas;
+        private int[] v;
+        private int c;
+        public void Iniciar(int numBolas)
+        {
+            // Número de bolas
+            this.numBolas = numBolas;
+            // Número de bolas inseridas no vetor
+            int k = 0;
+            // Vetor com as bolas sorteadas
+            v = new int[numBolas];
+            // Sorteia um número
+            Random r = new Random();
+            while (k < numBolas)
+            {
+                int n = r.Next(1, numBolas + 1);
+                // Procura o número no vetor
+                if (Array.IndexOf(v, n) == -1)
+                {
+                    v[k] = n; k++;
+                }
+            }
+        }
+        public int Proximo()
+        {
+            if (c < numBolas)
+            {
+                int x = v[c];
+                c++;
+                return x;
+            }
+            return -1;
+        }
     }
 }
